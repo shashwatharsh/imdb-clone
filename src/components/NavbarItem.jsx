82b2486d886ed401e9444c908e1f4a48ec9077aa
Shashwat Harsh <harshshashwat18@gmail.com>
2024-03-06 09:12:09 +0530

    create navbar component

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,27 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+
+  return (
+    <div className="">
+      <Link
+        href={`/?genre=${param}`}
+        className={`hover:text-amber-600 font-semibold ${
+          genre == param
+            ? "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+            : ""
+        }`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+};
+
+export default NavbarItem;
